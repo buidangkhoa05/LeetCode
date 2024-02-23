f90d22d2050ca6b4a3df02d70eae27ec9ad1235e
khoabui <buidangkhoa05@gmail.com>
2024-02-24 00:35:46 +0700

    daily practice

--- Program.cs
+++ Program.cs
@@ -3,10 +3,8 @@ using _net_version;
 using _net_version.Project;
 using Project;
 
-var input = new int[]
-    {
-        10,15,20
-    };
-var result = MinCostClimbingStairsProblem.MinCostClimbingStairs(input);
+var input = "abcd";
+var input2 = "bbcd";
+var result = BackspaceStringCompare.BackspaceCompare(input, input2);
 Console.WriteLine(result);
 Console.ReadLine();
---
+++ Project/BackspaceStringCompare.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _net_version.Project
+{
+    internal static class BackspaceStringCompare
+    {
+
+        public static bool BackspaceCompare(string s, string t)
+        {
+            var sStack = new Stack<char>();
+            var tStack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                PushToStack(c, ref sStack);
+            }
+
+            foreach (var c in t)
+            {
+                PushToStack(c, ref tStack);
+            }
+
+            if (sStack.Count != tStack.Count)
+                return false;
+
+            while (sStack.Count > 0)
+            {
+                if (sStack.Pop() != tStack.Pop())
+                    return false;
+            }
+            return true;
+        }
+
+        public static void PushToStack(char c, ref Stack<char> stack)
+        {
+
+            if (c == '#')
+            {
+                if (stack.Count > 0)
+                    stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+        //public static bool BackspaceCompare(string s, string t)
+        //{
+        //    int indexS = s.Length - 1;
+        //    int indexT = t.Length - 1;
+
+        //    while (indexS >= 0 || indexT >= 0)
+        //    {
+        //        indexS = CheckBackSpaceChar(s, indexS);
+
+        //        indexT = CheckBackSpaceChar(t, indexT);
+
+        //        if (indexS >= 0 && indexT >= 0 && s[indexS] != t[indexT])
+        //            return false;
+        //        else if (indexS < 0 && indexT < 0)
+        //            break;
+
+        //        if (indexS >= 0)
+        //            indexS--;
+        //        if (indexT >= 0)
+        //            indexT--;
+        //    }
+
+        //    return indexS < 0 && indexT < 0;
+        //}
+
+        //public static int CheckBackSpaceChar(string s, int index)
+        //{
+        //    if (index < 0)
+        //        return index;
+
+        //    if (s[index] == '#')
+        //    {
+        //        var count = 1;
+        //        while (s[index - count] == '#')
+        //        {
+        //            count++;
+        //        }
+        //        index -= count * 2;
+        //    }
+
+        //    return index;
+        //}
+    }
+}
