473049743573e8666342b50f0cc61ffa822a8dad
khoabui <buidangkhoa05@gmail.com>
2024-02-24 20:13:40 +0700

    daily problem

--- Program.cs
+++ Program.cs
@@ -3,8 +3,15 @@ using _net_version;
 using _net_version.Project;
 using Project;
 
-var input = 19;
-//var input2 = "bbcd";
-var result = HappyNumber.IsHappy(input);
+var input = new ListNode(0);
+//input.next = new ListNode(9);
+//input.next.next = new ListNode(9);
+//input.next.next.next = new ListNode(9);
+
+var input2 = new ListNode(7);
+input2.next = new ListNode(3);
+//input2.next.next = new ListNode(9);
+
+var result = AddTwoNumber.AddTwoNumbers(input, input2);
 Console.WriteLine(result);
 Console.ReadLine();
---
+++ Project/AddTwoNumber.cs
@@ -0,0 +1,43 @@
+﻿using Project;
+
+namespace _net_version.Project
+{
+    internal static class AddTwoNumber
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var tmp = 0;
+            var headNode = l1;
+
+            while (l1 != null || l2 != null)
+            {
+                l1 ??= new ListNode(0);
+                l2 ??= new ListNode(0);
+
+                tmp += l1.val + l2.val;
+
+                if (tmp >= 10)
+                {
+                    l1.val = tmp % 10;
+                    tmp = 1;
+                }
+                else
+                {
+                    l1.val = tmp;
+                    tmp = 0;
+                }
+
+                if ((tmp != 0 && l1.next == null) 
+                    || (l1.next == null && l2.next != null))
+                {
+                    l1.next = new ListNode(0);
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }    
+
+            return headNode;
+        }
+    }
+}
